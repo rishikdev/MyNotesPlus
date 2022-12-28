4da3c584ab659e12e7c20524da19cd3ec953bb3c
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2022-12-28 15:10:54 -0600

    UPDATED Website

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const features = [
     "Ability to pin notes",
     "Coloured tags that help in organising notes more efficiently",
     "Ability to switch between list view and card view",
+    "Ability to filter notes based on the tags",
     "Ability to sort notes either by Date or by Title in ascending as well as descending order",
     "Customisable widget support",
     "Apple watch application with the ability to create, read, and delete notes",
@@ -41,7 +42,7 @@ const application_description = [
 
     {
         card_title : "What you pay",
-        card_text : "This is the best part! You pay NOTHING! My Notes is a completely free application, and yes, all the updates in the future are also going to be free. You will not be pestered by any annoying ads either as showing any ads might compromise your data.</p>"
+        card_text : "This is the best part! You pay NOTHING! My Notes Plus is a completely free application, and yes, all the updates in the future are also going to be free. You will not be pestered by any annoying ads either as showing any ads might compromise your data.</p>"
     },
 
     {
